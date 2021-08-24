759607e8a1bf2ff0b0c8fdc5330892a681874a1e
Mike Ball <mike.ball3@gmail.com>
2021-08-24 13:59:30 -0400

    adding some stuff

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 module.exports.pg = require('pg');
 module.exports.connect = require('connect');
-module.exports.redis = require('redis')
-//redis
-//cookie parser
+module.exports.redis = require('redis');
+module.exports.bodyParser = require('body-parser');
+module.exports.cookieParser = require('cookie-parser');
