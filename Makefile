06f0543cf5c2510ee803357bbb5dbc37f3b5aae3
Mike Ball <mike.ball3@gmail.com>
2021-12-06 10:05:19 -0500

    updates

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
+
+.PHONY: clean, install, build
+
 clean:
 	rm -rf ./dist
 
 build:
 	node_modules/.bin/webpack --mode production
+	
+install:
+	cp dist/mstdlib.js /Users/mike/projects/demo0/aws_lambda/app_tracking/mstdlib.js
--- main.js
+++ main.js
@@ -3,3 +3,5 @@ module.exports.connect = require('connect');
 module.exports.redis = require('redis');
 module.exports.bodyParser = require('body-parser');
 module.exports.cookieParser = require('cookie-parser');
+module.exports.pgPromise = require('pg-promise');
+//TODO: https://github.com/scopsy/await-to-js#readme?
